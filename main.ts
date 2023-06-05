b39e9829be043c98bba5a8cf6ffe77c38f82906d
alan4390 <135559669+alan4390@users.noreply.github.com>
2023-06-04 21:00:43 -0300

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,21 @@
-input.onButtonPressed(Button.A, function () {
-	
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showNumber(49 + 57)
 })
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.East)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        # # # # .
+        # . . . #
+        . # . # #
+        . # # # .
+        . # . # .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(input.temperature())
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(input.lightLevel())
 })
